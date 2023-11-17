57a71100b5d511df42a04d26b287b2357380930a
Leshanu E <leshanu.e@maverick-dc.com>
2023-11-17 18:50:01 +0300

    Fix transmit correlationId by header

---
+++ README.md
@@ -0,0 +1,5 @@
+### About plugin
+
+### Install
+
+### Configure
--- correlation.go
+++ correlation.go
@@ -8,6 +8,10 @@ import (
 	"os"
 )
 
+const (
+	DEFAULT_HEADER_NAME = "correlation-id"
+)
+
 var (
 	logDebug = os.Stdout.WriteString
 	//logError = os.Stderr.WriteString
@@ -15,43 +19,38 @@ var (
 
 // Config the plugin configuration.
 type Config struct {
-	Headers []string `yaml:"headers,omitempty" json:"headers,omitempty"`
+	HeaderName string `yaml:"headerName,omitempty" json:"header_name,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		Headers: []string{},
+		HeaderName: DEFAULT_HEADER_NAME,
 	}
 }
 
 type Correlation struct {
-	next    http.Handler
-	name    string
-	headers map[string]bool
+	next       http.Handler
+	name       string
+	headerName string
 }
 
 // New created a new plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	var headers = make(map[string]bool, len(config.Headers))
-
-	if len(config.Headers) > 0 {
-		logDebug(fmt.Sprintf("Finded %d count headers at request", len(config.Headers)))
-		for _, name := range config.Headers {
-			logDebug(fmt.Sprintf("Incoming header by name: %s", name))
-			headers[name] = true
-		}
-	}
-
 	return &Correlation{
-		headers: headers,
-		next:    next,
-		name:    name,
+		next:       next,
+		name:       name,
+		headerName: config.HeaderName,
 	}, nil
 }
 
 func (c *Correlation) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	var id = uuid.NewV4().String()
+	writer.Header().Add(c.headerName, id)
+	if request.Header.Get(c.headerName) != "" {
+		writer.Header().Add(c.headerName, request.Header.Get(c.headerName))
+	}
+
 	logDebug(fmt.Sprintf("All headers are incoming with correlationId: %s", id))
 	c.next.ServeHTTP(writer, request)
 }
