e0526f62d031f3024dfd69a2e252e8e290efcf9c
Leshanu E <leshanu.e@maverick-dc.com>
2023-11-18 21:07:08 +0300

    Add log to serve headers

--- correlation.go
+++ correlation.go
@@ -4,18 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/satori/go.uuid"
-	"log"
 	"net/http"
-	"os"
 )
 
 const (
 	DEFAULT_HEADER_NAME = "correlation-id"
-)
-
-var (
-	logDebug = os.Stdout.WriteString
-	logError = os.Stderr.WriteString
 )
 
 // Config the plugin configuration.
@@ -39,7 +32,7 @@ type Correlation struct {
 // New created a new plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	if config.HeaderName == "" {
-		logError("[Correlation] headerName option cannot be empty")
+		fmt.Println("[Correlation] headerName option cannot be empty")
 		return nil, fmt.Errorf("[Correlation] headerName option cannot be empty")
 	}
 
@@ -51,19 +44,15 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 }
 
 func (c *Correlation) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	logDebug("[Correlation][StdOut] Try to serve next")
-	fmt.Println("[Correlation][Fmt] Try to serve next")
-	log.Println("[Correlation][Log] Try to serve next")
-	var id = uuid.NewV4().String()
+	fmt.Println("[Correlation][Fmt] Try to serve next", c.headerName, request.Header.Get(c.headerName))
 
+	var id = uuid.NewV4().String()
 	request.Header.Add(c.headerName, id)
 	if request.Header.Get(c.headerName) != "" {
-		logDebug(fmt.Sprintf("[Correlation] HeaderName by value %s is empty", c.headerName))
+		fmt.Printf("[Correlation] HeaderName by value %s is empty", c.headerName)
 		request.Header.Add(c.headerName, request.Header.Get(c.headerName))
 	}
 
-	fmt.Printf("[Correlation][Fmt] All headers are incoming with correlationId: %s\n", id)
-	log.Printf("[Correlation][Log] All headers are incoming with correlationId: %s\n", id)
-	logDebug(fmt.Sprintf("[Correlation] All headers are incoming with correlationId: %s", id))
+	fmt.Printf("[Correlation][Fmt] All headers are incoming with correlationId: %s", id)
 	c.next.ServeHTTP(writer, request)
 }
