a7ff189da87c1fa94da2da281973234714d7ed56
Leshanu E <leshanu.e@maverick-dc.com>
2023-11-18 20:15:24 +0300

    Fix debug error for middleware

--- correlation.go
+++ correlation.go
@@ -3,13 +3,18 @@ package traefik_correlation
 import (
 	"context"
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"github.com/satori/go.uuid"
 	"net/http"
+	"os"
 )
 
 const (
 	DEFAULT_HEADER_NAME = "correlation-id"
+)
+
+var (
+	logDebug = os.Stdout.WriteString
+	logError = os.Stderr.WriteString
 )
 
 // Config the plugin configuration.
@@ -33,6 +38,7 @@ type Correlation struct {
 // New created a new plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	if config.HeaderName == "" {
+		logError("[Correlation] headerName option cannot be empty")
 		return nil, fmt.Errorf("[Correlation] headerName option cannot be empty")
 	}
 
@@ -44,14 +50,14 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 }
 
 func (c *Correlation) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	log.Debug().Msg("[Correlation] Try to serve next")
+	logDebug("[Correlation] Try to serve next")
 	var id = uuid.NewV4().String()
 
 	if request.Header.Get(c.headerName) == "" {
-		log.Debug().Msgf("[Correlation] HeaderName by value %s is empty", c.headerName)
+		logDebug(fmt.Sprintf("[Correlation] HeaderName by value %s is empty", c.headerName))
 		request.Header.Add(c.headerName, id)
 	}
 
-	log.Debug().Msgf("[Correlation] All headers are incoming with correlationId: %s", id)
+	logDebug(fmt.Sprintf("[Correlation] All headers are incoming with correlationId: %s", id))
 	c.next.ServeHTTP(writer, request)
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,6 @@ module github.com/killer-djon/traefik-correlation
 
 go 1.20
 
-require (
-	github.com/rs/zerolog v1.31.0
-	github.com/satori/go.uuid v1.2.0
-)
+require github.com/satori/go.uuid v1.2.0
 
-require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-)
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
